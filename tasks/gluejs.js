cce0544352a62fa76c2e9aed4e8c401194b54adb
Marco Pantaleoni <marco.pantaleoni@gmail.com>
2014-04-07 15:38:33 +0200

    Support gluejs v2 and add support for 'transform', 'command' and 'replace'.
    Add support also for 'cache', 'require', 'report', 'main'.

--- tasks/gluejs.js
+++ tasks/gluejs.js
@@ -14,17 +14,18 @@ module.exports = function(grunt) {
 
 	grunt.registerMultiTask('gluejs', 'Build CommonJS modules for the browser', function() {
 		// merges task-specific and/or target-specific options with these defaults.
+		var done = this.async();
+
 		var options = this.options({
-			stripBanners: false,
-			process: false
+			cache: false,
+			require: false,
+			report: false,
+			debug: false
 		});
 
-		// normalizes boolean options that accept options objects.
-		if (options.stripBanners === true) { options.stripBanners = {}; }
-		if (options.process === true) { options.process = {}; }
-
-		// default options for glue
-		Glue.defaults(options);
+		var n_todo = 0;
+		var n_done = 0;
+		var n_err  = 0;
 
 		// processes banner and footer.
 		var banner = options.banner ? grunt.template.process(options.banner) : '';
@@ -32,27 +33,88 @@ module.exports = function(grunt) {
 
 		// iterates over all src-dest file pairs.
 		this.files.forEach(function(file) {
+			n_todo = n_todo + 1;
+
 			var glue = new Glue();
 
-			// export
-			if (options.export) { glue.export(options.export); }
+			if (options.debug) {
+				grunt.log.writeln("processing file '" + file.src + "'");
+			}
+
+			if (options.cache !== null) { glue.set('cache', options.cache); }
+			if (options.require !== null) { glue.set('require', options.require); }
+			if (options.report !== null) { glue.set('report', options.report); }
+			//glue.set('cache', false).set('require', false).set('report', true);
+
+			// command
+			if (options.command) { glue.set('command', options.command); }
+
+			// transform
+			if (options.transform) {
+				glue.options['transform'] = options.transform;
+			}
+
 
 			// basepath
-			// TODO: should work with defaults
-			if (options.basepath) { glue.basepath(options.basepath); }
+			if (options.basepath) {
+				// output paths are relative to this
+				glue.basepath(options.basepath);
+			} else {
+				// output paths are relative to this
+				glue.basepath(process.cwd());
+			}
 
 			// includes source files - excludes the destination file if it is in the same directory of source(s) file(s)
 			file.src.forEach(function(src) {
 				if (src !== file.dest) {
+					// includes all files in the dir
 					glue.include(src);
 				}
 			});
 
-			glue.render(function(err, output) {
-				output = banner + output + footer;
-				grunt.file.write(file.dest, output);
+			// exclude
+			if (options.exclude) { glue.exclude(options.exclude); }
 
-				grunt.log.writeln('File "' + file.dest + '" created.');
+			if (options.main) {
+				// the file that's exported as the root of the package
+				glue.main(options.main);
+			}
+
+			// export
+			if (options.export) { glue.export(options.export); }
+
+			// replace
+			if (options.replace) {
+				var obj = options.replace;
+				for (var key in obj) {
+					if (obj.hasOwnProperty(key)) {
+						glue.replace(key, obj[key]);
+					}
+				}
+			}
+
+			if (options.debug) {
+				grunt.log.writeln("going to render to '" + file.dest + "'");
+				console.log(glue);
+			}
+			glue.render(function(err, output) {
+				if (err) {
+					grunt.log.writeln("error:", err);
+					grunt.warn.writeln('File "' + file.dest + '" NOT created.');
+
+					n_err++;
+				} else {
+					output = banner + output + footer;
+					grunt.file.write(file.dest, output);
+
+					grunt.log.writeln('File "' + file.dest + '" created.');
+				}
+
+				n_done++;
+				if (n_todo === n_done) {
+					// All done!
+					done((n_err === 0));
+				}
 			});
 		});
 	});
